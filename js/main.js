d6084cb9dd1541ceaad00f60ec840f639be6d777
Kum Jude Tem <judekum14@gmail.com>
2021-04-25 13:01:43 +0100

    Carousel slide section added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 // alert("Working");
+$(document).ready(function () {
+  $("#slider5").bsTouchSlider();
+  $(".carousel .carousel-inner").swipe({
+    swipeLeft: function (event, direction, distance, duration, fingerCount) {
+      this.parent().carousel("next");
+    },
+    swipeRight: function () {
+      this.parent().carousel("prev");
+    },
+    threshold: 0,
+  });
+});
