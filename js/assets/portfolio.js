da80584708f928f63e6c931b354686559e7f521d
Kum Jude Tem <judekum14@gmail.com>
2021-04-26 16:24:49 +0100

    Testimonials section added

--- js/assets/portfolio.js
+++ js/assets/portfolio.js
@@ -1,15 +1,12 @@
 $(function () {
-    "use strict";
-                // init Isotope
-                var $grid = $('.gallery-box').isotope({
-                    itemSelector: '.g-item',
-                    percentPosition: true,
-                    masonry: {
-                        // use outer width of grid-sizer for columnWidth
-                        columnWidth: '.g-item',
-                    }
-                });
-                
-
-                  
+  "use strict";
+  // init Isotope
+  let $grid = $(".gallery-box").isotope({
+    itemSelector: ".g-item",
+    percentPosition: true,
+    masonry: {
+      // use outer width of grid-sizer for columnWidth
+      columnWidth: ".g-item",
+    },
+  });
 });
--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
 // alert("Working");
+
+$(function () {
+  $("#slider5").bsTouchSlider();
+  $(".carousel .carousel-inner").swipe({
+    swipeLeft: function (event, direction, distance, duration, fingerCount) {
+      this.parent().carousel("next");
+    },
+    swipeRight: function () {
+      this.parent().carousel("prev");
+    },
+    threshold: 0,
+  });
+  // This is for the static slider 6
+  $(".video-img").on("click", function () {
+    $(this).addClass("hide");
+    $(".embed-responsive").show().removeClass("hide");
+    $("video").each(function () {
+      this.play();
+    });
+  });
+  //testimonial 9
+  $(".testi9").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1650: {
+        items: 1,
+      },
+    },
+  });
+});
